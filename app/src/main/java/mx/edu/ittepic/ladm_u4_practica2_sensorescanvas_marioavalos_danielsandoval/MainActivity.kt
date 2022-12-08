d6807a530ff0d0bbac4ce77e0c28aefaca2d4bcb
Dany Sandoval <dasandovalal@ittepic.edu.mx>
2022-12-07 19:35:59 -0700

    First Commit

---
+++ app/src/main/java/mx/edu/ittepic/ladm_u4_practica2_sensorescanvas_marioavalos_danielsandoval/MainActivity.kt
@@ -0,0 +1,11 @@
+package mx.edu.ittepic.ladm_u4_practica2_sensorescanvas_marioavalos_danielsandoval
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
